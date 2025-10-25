1cf26d1c7180f85a34930bf88ef24baccbdd5828
Navio1430 <naviodev@gmail.com>
2025-10-25 21:05:02 +0200

    remove unused javax persistence api dependency

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -7,7 +7,6 @@ dependencies {
 
   implementation("com.zaxxer:HikariCP:7.0.2")
   implementation("com.j256.ormlite:ormlite-jdbc:6.1")
-  implementation("javax.persistence:javax.persistence-api:2.2")
 
   testRuntimeOnly("com.h2database:h2:2.4.240")
 }
