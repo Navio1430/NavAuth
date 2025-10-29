9959260786087633517907c477d78ee72dc84324
Navio1430 <naviodev@gmail.com>
2025-10-29 02:09:29 +0100

    add HttpClient module

---
+++ common/src/main/kotlin/pl/spcode/navauth/common/module/HttpClientModule.kt
@@ -0,0 +1,32 @@
+/*
+ * NavAuth
+ * Copyright © 2025 Oliwier Fijas (Navio1430)
+ *
+ * NavAuth is free software; You can redistribute it and/or modify it under the terms of:
+ * the GNU Affero General Public License version 3 as published by the Free Software Foundation.
+ *
+ * NavAuth is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with NavAuth. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU Affero General Public License.
+ *
+ */
+
+package pl.spcode.navauth.common.module
+
+import com.google.inject.AbstractModule
+import com.google.inject.Provides
+import java.net.http.HttpClient
+
+class HttpClientModule: AbstractModule() {
+
+  @Provides
+  fun provideHttpClient(): HttpClient {
+    return HttpClient.newHttpClient()
+  }
+
+}
