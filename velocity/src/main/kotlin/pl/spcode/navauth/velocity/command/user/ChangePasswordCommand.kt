4488d535abb002d9064cf60eb5909dccdc77bbb1
Navio1430 <naviodev@gmail.com>
2025-11-05 10:46:24 +0100

    velocity: impl change password command

--- velocity/src/main/kotlin/pl/spcode/navauth/velocity/command/user/ChangePasswordCommand.kt
+++ velocity/src/main/kotlin/pl/spcode/navauth/velocity/command/user/ChangePasswordCommand.kt
@@ -18,24 +18,48 @@
 
 package pl.spcode.navauth.velocity.command.user
 
+import com.google.inject.Inject
 import com.velocitypowered.api.proxy.Player
 import dev.rollczi.litecommands.annotations.argument.Arg
+import dev.rollczi.litecommands.annotations.async.Async
 import dev.rollczi.litecommands.annotations.command.Command
 import dev.rollczi.litecommands.annotations.context.Context
 import dev.rollczi.litecommands.annotations.execute.Execute
 import dev.rollczi.litecommands.annotations.permission.Permission
+import net.kyori.adventure.text.Component
+import pl.spcode.navauth.common.application.credentials.UserCredentialsService
+import pl.spcode.navauth.common.application.user.UserService
+import pl.spcode.navauth.common.domain.credentials.UserCredentials
+import pl.spcode.navauth.common.infra.crypto.BCryptCredentialsHasher
 import pl.spcode.navauth.velocity.command.Permissions
+import pl.spcode.navauth.velocity.component.TextColors
 
 @Command(name = "changepassword")
 @Permission(Permissions.USER_CHANGE_PASSWORD)
-class ChangePasswordCommand {
+class ChangePasswordCommand @Inject constructor(val userService: UserService, val userCredentialsService: UserCredentialsService) {
 
+  @Async
   @Execute
-  fun login(
+  fun changePassword(
     @Context sender: Player,
     @Arg(value = "current_password") currentPassword: String,
     @Arg(value = "new_password") newPassword: String,
   ) {
-    // todo impl
+    val user = userService.findUserByUsername(sender.username, false)!!;
+    if (user.isPremium) {
+      sender.sendMessage(Component.text("Can't execute this command right now: your account is set to premium mode.", TextColors.RED))
+      return;
+    }
+    val credentials = userCredentialsService.findCredentials(user)!!
+    val isCorrectPassword = userCredentialsService.verifyPassword(credentials, currentPassword)
+    if (!isCorrectPassword) {
+      sender.sendMessage(Component.text("Wrong password!", TextColors.RED))
+      return;
+    }
+
+    val newCredentials = UserCredentials.create(user, BCryptCredentialsHasher().hash(newPassword))
+    userCredentialsService.storeUserCredentials(newCredentials)
+
+    sender.sendMessage(Component.text("Success! New password set.", TextColors.GREEN))
   }
 }
