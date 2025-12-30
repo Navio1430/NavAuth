b950ebe83e3357d0aa9a7212eef6b33f8a4e0e8f
Navio1430 <naviodev@gmail.com>
2025-12-30 01:49:20 +0100

    comment out unused methods in PreAuthenticatedUserService

--- navauth-common/src/main/kotlin/pl/spcode/navauth/common/application/auth/handshake/PreAuthenticatedUserService.kt
+++ navauth-common/src/main/kotlin/pl/spcode/navauth/common/application/auth/handshake/PreAuthenticatedUserService.kt
@@ -29,67 +29,67 @@ class PreAuthenticatedUserService
 @Inject
 constructor(val userService: UserService, val profileService: MojangProfileService) {
 
-  /** @param connectionUsername initial connection username */
-  fun validateUsername(connectionUsername: String): Boolean {
-    // todo check regex
-    return true
-  }
-
-  fun getPreAuthUserState(connUsername: Username): PreAuthUserState {
-    val user = userService.findUserByUsernameIgnoreCase(connUsername.value)
-
-    var existingUser = userService.findUserByUsernameIgnoreCase(connUsername.value)
-    val userExists = existingUser != null
-    val correspondingPremiumProfile = profileService.fetchProfileInfo(connUsername)
-    val isPremiumNickname = correspondingPremiumProfile != null
-
-    val state: PreAuthUsernameState = return PreAuthUserState(getUsernameState())
-  }
-
-  private fun getUsernameState(connUsername: String, existingUser: User?): PreAuthUsernameState {
-    if (existingUser != null) {
-      if (existingUser.isPremium) {
-        // user could change 1 letter to be uppercased/lowercased in their nickname
-        if (connUsername != existingUser.username.value) {
-          // let them through and make data migration later after auth (NOT BEFORE)
-          return PreAuthUsernameState.POSSIBLE_PREMIUM_USERNAME_CHANGE
-        }
-      }
-      // non premium user
-      else {
-        if (isPremiumNickname) {
-          if (correspondingPremiumProfile.name == existingUser.username.value) {
-            session.usernameState = PreAuthUsernameState.USERNAME_POTENTIAL_CONFLICT
-            if (connUsername != correspondingPremiumProfile.name) {
-              event.result =
-                usernameRequiredDeniedResult(connUsername, correspondingPremiumProfile.name)
-              return
-            }
-          } else {
-            // todo refactor conflicts
-            session.usernameState = PreAuthUsernameState.USERNAME_CONFLICT
-            event.result = usernameConflictDeniedResult(correspondingPremiumProfile.name)
-            return
-          }
-        }
-        // not a premium nickname
-        else {
-          if (connUsername != existingUser.username.value) {
-            event.result = usernameRequiredDeniedResult(connUsername, existingUser.username.value)
-            return
-          }
-        }
-      }
-    }
-    // user doesn't exist yet
-    else {
-      if (isPremiumNickname) {
-        if (connUsername != correspondingPremiumProfile.name) {
-          event.result =
-            premiumUsernameRequiredDeniedResult(connUsername, correspondingPremiumProfile.name)
-          return
-        }
-      }
-    }
-  }
+//  /** @param connectionUsername initial connection username */
+//  fun validateUsername(connectionUsername: String): Boolean {
+//    // todo check regex
+//    return true
+//  }
+//
+//  fun getPreAuthUserState(connUsername: Username): PreAuthUserState {
+//    val user = userService.findUserByUsernameIgnoreCase(connUsername.value)
+//
+//    var existingUser = userService.findUserByUsernameIgnoreCase(connUsername.value)
+//    val userExists = existingUser != null
+//    val correspondingPremiumProfile = profileService.fetchProfileInfo(connUsername)
+//    val isPremiumNickname = correspondingPremiumProfile != null
+//
+//    val state: PreAuthUsernameState = return PreAuthUserState(getUsernameState())
+//  }
+//
+//  private fun getUsernameState(connUsername: String, existingUser: User?): PreAuthUsernameState {
+//    if (existingUser != null) {
+//      if (existingUser.isPremium) {
+//        // user could change 1 letter to be uppercased/lowercased in their nickname
+//        if (connUsername != existingUser.username.value) {
+//          // let them through and make data migration later after auth (NOT BEFORE)
+//          return PreAuthUsernameState.POSSIBLE_PREMIUM_USERNAME_CHANGE
+//        }
+//      }
+//      // non premium user
+//      else {
+//        if (isPremiumNickname) {
+//          if (correspondingPremiumProfile.name == existingUser.username.value) {
+//            session.usernameState = PreAuthUsernameState.USERNAME_POTENTIAL_CONFLICT
+//            if (connUsername != correspondingPremiumProfile.name) {
+//              event.result =
+//                usernameRequiredDeniedResult(connUsername, correspondingPremiumProfile.name)
+//              return
+//            }
+//          } else {
+//            // todo refactor conflicts
+//            session.usernameState = PreAuthUsernameState.USERNAME_CONFLICT
+//            event.result = usernameConflictDeniedResult(correspondingPremiumProfile.name)
+//            return
+//          }
+//        }
+//        // not a premium nickname
+//        else {
+//          if (connUsername != existingUser.username.value) {
+//            event.result = usernameRequiredDeniedResult(connUsername, existingUser.username.value)
+//            return
+//          }
+//        }
+//      }
+//    }
+//    // user doesn't exist yet
+//    else {
+//      if (isPremiumNickname) {
+//        if (connUsername != correspondingPremiumProfile.name) {
+//          event.result =
+//            premiumUsernameRequiredDeniedResult(connUsername, correspondingPremiumProfile.name)
+//          return
+//        }
+//      }
+//    }
+//  }
 }
