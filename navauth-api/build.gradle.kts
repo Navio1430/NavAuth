13860356e6991c36f6398abde3daf6359048a7a8
Navio1430 <naviodev@gmail.com>
2026-01-08 23:23:59 +0100

    feat: add GitHub Actions for releasing and publishing artifacts, configure Maven publishing

--- navauth-api/build.gradle.kts
+++ navauth-api/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
   kotlin("jvm")
+  id("maven-publish")
 }
 
 group = "pl.spcode.navauth.api"
-version = "0.1.0-SNAPSHOT"
 
 repositories {
   mavenCentral()
@@ -13,6 +13,20 @@ dependencies {
   compileOnly(libs.velocitypowered.velocity.api)
 }
 
-kotlin {
-  jvmToolchain(21)
+publishing {
+  publications {
+    create<MavenPublication>("gpr") {
+      from(components["java"])
+    }
+  }
+  repositories {
+    maven {
+      name = "GitHubPackages"
+      url = uri("https://maven.pkg.github.com/Navio1430/NavAuth")
+      credentials {
+        username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user") as String? ?: ""
+        password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.key") as String? ?: ""
+      }
+    }
+  }
 }
