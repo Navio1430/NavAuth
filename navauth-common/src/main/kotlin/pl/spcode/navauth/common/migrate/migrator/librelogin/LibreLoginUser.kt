1345619bc105897ce01c3cf93e14c89d7e27e614
Navio1430 <naviodev@gmail.com>
2026-01-11 20:19:13 +0100

    fix: replace `Date` with `Timestamp` in LibreLoginUser for database fields

--- navauth-common/src/main/kotlin/pl/spcode/navauth/common/migrate/migrator/librelogin/LibreLoginUser.kt
+++ navauth-common/src/main/kotlin/pl/spcode/navauth/common/migrate/migrator/librelogin/LibreLoginUser.kt
@@ -21,7 +21,7 @@ package pl.spcode.navauth.common.migrate.migrator.librelogin
 import com.j256.ormlite.field.DataType
 import com.j256.ormlite.field.DatabaseField
 import com.j256.ormlite.table.DatabaseTable
-import java.util.Date
+import java.sql.Timestamp
 import java.util.UUID
 
 @DatabaseTable(tableName = "librepremium_data")
@@ -33,13 +33,14 @@ class LibreLoginUser(
   @DatabaseField(canBeNull = true, columnName = "salt") var passwordSalt: String? = null,
   @DatabaseField(canBeNull = true, columnName = "algo") var passwordAlgo: String? = null,
   @DatabaseField(canBeNull = false, columnName = "last_nickname") var lastNickname: String? = null,
-  @DatabaseField(dataType = DataType.DATE_LONG, columnName = "joined") var joinDate: Date? = null,
-  @DatabaseField(dataType = DataType.DATE_LONG, columnName = "last_seen")
-  var lastSeen: Date? = null,
+  @DatabaseField(dataType = DataType.TIME_STAMP, columnName = "joined")
+  var joinDate: Timestamp? = null,
+  @DatabaseField(dataType = DataType.TIME_STAMP, columnName = "last_seen")
+  var lastSeen: Timestamp? = null,
   @DatabaseField(canBeNull = true, columnName = "secret") var secret: String? = null,
   @DatabaseField(canBeNull = true, columnName = "ip") var ip: String? = null,
-  @DatabaseField(dataType = DataType.DATE_LONG, columnName = "last_authentication")
-  var lastAuthentication: Date? = null,
+  @DatabaseField(dataType = DataType.TIME_STAMP, columnName = "last_authentication")
+  var lastAuthentication: Timestamp? = null,
   @DatabaseField(canBeNull = true, columnName = "last_server") var lastServer: String? = null,
   @DatabaseField(canBeNull = true, columnName = "email") var email: String? = null,
 )
