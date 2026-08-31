46a970c8682150f2b8ee711706ae328ece33527a
Navio1430 <naviodev@gmail.com>
2026-08-31 07:02:53 +0200

    chore: use libs.versions.toml for all deps

--- navauth-common/build.gradle.kts
+++ navauth-common/build.gradle.kts
@@ -1,45 +1,43 @@
 
 dependencies {
 
-  // todo: move dependencies to libs.toml
-
   api(project(":navauth-api"))
 
   // EternalCode Multification
-  api("com.eternalcode:multification-core:1.2.3")
-  api("com.eternalcode:multification-okaeri:1.2.3")
+  api(libs.multification.core)
+  api(libs.multification.okaeri)
 
-  implementation("com.google.inject:guice:7.0.0")
-  api("com.google.code.gson:gson:2.13.2")
+  implementation(libs.guice)
+  api(libs.gson)
 
   // database
-  implementation("com.zaxxer:HikariCP:7.0.2")
-  api("com.j256.ormlite:ormlite-jdbc:6.1")
+  implementation(libs.hikaricp)
+  api(libs.ormlite.jdbc)
 
   // config
-  api("eu.okaeri:okaeri-configs-yaml-snakeyaml:6.0.0-beta.27")
+  api(libs.okaeri.configs.yaml.snakeyaml)
 
   // crypto
-  implementation("at.favre.lib:bcrypt:0.10.2")
-  implementation("org.bouncycastle:bcprov-jdk18on:1.83")
-  implementation("org.bouncycastle:bcutil-jdk18on:1.83")
+  implementation(libs.bcrypt)
+  implementation(libs.bouncycastle.prov)
+  implementation(libs.bouncycastle.util)
 
-  compileOnly("net.kyori:adventure-text-minimessage:4.25.0")
+  compileOnly(libs.adventure.text.minimessage)
 
   // drivers
-  runtimeOnly("com.h2database:h2:2.4.240")
-  runtimeOnly("com.mysql:mysql-connector-j:9.5.0")
-  runtimeOnly("org.postgresql:postgresql:42.7.8")
-  runtimeOnly("org.xerial:sqlite-jdbc:3.51.1.0")
+  runtimeOnly(libs.h2)
+  runtimeOnly(libs.mysql)
+  runtimeOnly(libs.postgresql)
+  runtimeOnly(libs.sqlite.jdbc)
 
   // litecommands core (compileOnly because it is platform-dependent)
   compileOnly(libs.litecommands.core)
 
   // qr code generation
-  api("com.google.zxing:core:3.5.4")
+  api(libs.zxing.core)
 
   // tests
-  testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
-  testImplementation("io.kotest:kotest-assertions-core:5.8.0")
-  testImplementation("io.mockk:mockk:1.13.8")
+  testImplementation(libs.kotest.runner.junit5)
+  testImplementation(libs.kotest.assertions.core)
+  testImplementation(libs.mockk)
 }
