bdaba072bbbc50e4ae3da69f79c98cdf41931343
Navio1430 <naviodev@gmail.com>
2026-09-01 16:29:03 +0200

    update gradle to 9.7.1, update to java 25, update to velocity 4.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
   id("java")
-  kotlin("jvm") version "2.2.21"
+  kotlin("jvm") version "2.4.10"
   alias(libs.plugins.spotless)
 }
 
@@ -43,12 +43,15 @@ subprojects {
   }
 
   kotlin {
-    jvmToolchain(21)
+    jvmToolchain(25)
   }
 
   java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    toolchain {
+      languageVersion = JavaLanguageVersion.of(25)
+      targetCompatibility = JavaVersion.VERSION_25
+      sourceCompatibility = JavaVersion.VERSION_25
+    }
   }
 
   tasks.test {
